1841fd1a7024f548bd6a5adb5d3a5753bc82752f
Aymen-Mohammed7 <aymenmuaamar@gmail.com>
2026-06-29 04:43:28 +0300

    Update Exploratory_Data_Analysis.sql

--- SQL scripts/Exploratory_Data_Analysis.sql
+++ SQL scripts/Exploratory_Data_Analysis.sql
@@ -1,58 +1,69 @@
 -- Exploratory Data Analysis
 
 
+-- Max & Min layoffs
 SELECT MAX(total_laid_off), MAX(percentage_laid_off), MAX(funds_raised_millions)
 FROM layoffs_staging2;
 
 
+-- Companies with 100% layoffs
 SELECT *
 FROM layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY total_laid_off DESC;
 
 
+-- Total layoffs by company
 SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY SUM(total_laid_off) DESC;
 
 
+-- Data boundries
 SELECT MIN(`date`), MAX(`date`)
 FROM layoffs_staging2;
 
 
+-- Total layoffs by industry
 SELECT industry, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY industry
 ORDER BY SUM(total_laid_off) DESC;
 
 
+-- All data
 SELECT *
 FROM layoffs_staging2;
 
 
+-- Total layoffs by country
 SELECT country, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY country
 ORDER BY SUM(total_laid_off) DESC;
 
 
+-- Total layoffs by year
 SELECT YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY YEAR(`date`)
 ORDER BY YEAR(`date`) DESC;
 
 
+-- Total layoffs
 SELECT SUM(total_laid_off)
 FROM layoffs_staging2;
 
 
+-- -- Total layoffs by stage
 SELECT stage, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY stage
 ORDER BY SUM(total_laid_off) DESC;
 
 
+-- Total layoffs by month/year
 SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY `Month`
@@ -60,6 +71,7 @@ HAVING `Month` IS NOT NULL
 ORDER BY `Month` ASC;
 
 
+-- Total rolling layoffs by month/year
 WITH Total_Rolling AS(
 SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off) AS total_offs
 FROM layoffs_staging2
@@ -71,12 +83,14 @@ SELECT `Month`, total_offs
 FROM Total_Rolling; 
 
 
+-- Total layoffs by company & year
 SELECT company, YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company, YEAR(`date`)
 ORDER BY SUM(total_laid_off) DESC;
 
 
+-- Rank of top 5 laid off companies by year
 WITH Company_Year (Company, `Year`, Total_laid_off) AS
 (
 SELECT company, YEAR(`date`), SUM(total_laid_off)
@@ -94,6 +108,7 @@ FROM Company_Rank
 WHERE Rank_by_total <= 5;
 
 
+-- Total percentage of layoffs by company
 SELECT company, SUM(percentage_laid_off)
 FROM layoffs_staging2
 GROUP BY company;
